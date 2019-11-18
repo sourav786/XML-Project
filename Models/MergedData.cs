8f98b4cd10a5b1d91fdbcd16ac39c6bcd3895efd
sourav786 <sourav77gupta@gmail.com>
2019-11-17 23:34:25 -0500

    model class changes

---
+++ Models/MergedData.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CombinedData
+{
+    public class MergedData
+    {
+
+        [JsonProperty("dbn")]
+        public string Dbn { get; set; }
+
+        [JsonProperty("school_name")]
+        public string SchoolName { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("location_name")]
+        public string LocationName { get; set; }
+
+
+    }
+}
